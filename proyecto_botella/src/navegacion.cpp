ee91563a620d66c40a6858d8812d13a3bde43c92
tapi1300 <pedop4@gmail.com>
2020-03-30 19:16:47 +0200

    Proyecto Botella acabado 200%

--- proyecto_botella/src/navegacion.cpp
+++ proyecto_botella/src/navegacion.cpp
@@ -1,26 +1,25 @@
 #include "actionlib/client/simple_action_client.h"
+#include <ctime>
 #include "darknet_ros_msgs/BoundingBoxes.h"
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Twist.h"
 #include "geometry_msgs/TransformStamped.h"
+#include <math.h>
 #include "move_base_msgs/MoveBaseAction.h"
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
+#include "sensor_msgs/LaserScan.h"
+#include <string>
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include "tf2/convert.h"
 #include "tf2_ros/transform_broadcaster.h"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2/transform_datatypes.h"
 #include "tf2/LinearMath/Transform.h"
-#include <math.h>
-#include <ctime>
-#include <string>
 #include <pcl_ros/transforms.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
 #include <pcl/point_types_conversion.h>
-#include "sensor_msgs/LaserScan.h"
-#include "ros/ros.h"
 
 
 
